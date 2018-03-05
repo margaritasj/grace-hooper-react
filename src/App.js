cf8c0a46e446edbb2913141e25b05ec48a7c4a05
margb13 <marg.b13@hotmail.com>
2018-03-05 09:52:02 -0500

    Añadiendo parrafos a componente content

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 
 // Components
+// import Content from './component/Header';
 import Content from './component/Content';
+// import Content from './component/Footer';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Content/>
+        <Content />
       </div>
     );
   }
